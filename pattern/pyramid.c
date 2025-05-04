394e5d6bc15b2f2cd3a4a0727f5b55c09bacc7e2
meherab nahin <nahinrahman87@gmail.com>
2025-05-04 16:40:16 +0600

    all pattern

--- pattern/pyramid.c
+++ pattern/pyramid.c
@@ -1,18 +1,23 @@
 #include<stdio.h>
 int main()
 {
-   int n,star=1;;
+   int n;
    scanf("%d",&n);
+   int star=(n*2)-1;
    int space=n-1;
-   for(int i=1;i<=n;i++){
-      for(int k=1;k<=space;k++){
+   for(int i=0;i<=n-1;i++){  // for line
+      for(int k=0;k<i;k++){  //for space
          printf(" ");
       }
-      for(int j=1;j<=star;j++){
-         printf("%d",j);
+      // for(int j='A';j<='A'+star;j++){ // for symbol(start,number,character,etc...)
+      //    printf("%c",j);
+      // }
+
+       for(int j=1;j<=star;j++){ // for symbol(start,number,character,etc...)
+         printf("*");
       }
     printf("\n");
-    star+=2;
+    star-=2;
     space--;
    }
 return 0;
